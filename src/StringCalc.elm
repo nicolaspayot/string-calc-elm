bdd1660bf51908558e1d829e4d52f3f6d95b6aaa
Nicolas Payot <nicolas.payot@gmail.com>
2017-06-07 14:10:06 +0200

    test: make test pass

--- src/StringCalc.elm
+++ src/StringCalc.elm
@@ -1,6 +1,17 @@
 module StringCalc exposing (..)
 
+import List
+
 add : String -> Int
-add numbers = String.toInt numbers
+add numbers =
+  if String.contains "," numbers then
+    String.split "," numbers
+    |> List.map toInt
+    |> List.sum
+  else
+    toInt numbers
+
+toInt : String -> Int
+toInt str = String.toInt str
   |> Result.toMaybe
   |> Maybe.withDefault 0
