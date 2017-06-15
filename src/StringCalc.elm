747871314b4821c5a9fa9d64d47b8e890069945f
Nicolas Payot <nicolas.payot@gmail.com>
2017-06-15 09:06:16 +0200

    test: refactor

--- src/StringCalc.elm
+++ src/StringCalc.elm
@@ -4,12 +4,16 @@ import List
 
 add : String -> Int
 add numbers =
-  if String.contains "," numbers then
-    String.split "," numbers
-    |> List.map toInt
-    |> List.sum
+  splitWithSeparator numbers ","
+  |> List.map toInt
+  |> List.sum
+
+splitWithSeparator : String -> String -> List String
+splitWithSeparator numbers separator =
+  if String.contains separator numbers then
+    String.split separator numbers
   else
-    toInt numbers
+    numbers :: []
 
 toInt : String -> Int
 toInt str = String.toInt str
