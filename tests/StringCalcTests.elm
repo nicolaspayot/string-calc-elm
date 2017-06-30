a31f92d1fa59bc8bc2c74dd37483c64ad9c99ed5
Nicolas Payot <nicolas.payot@gmail.com>
2017-06-30 14:54:52 +0200

    test: add failing test

--- tests/StringCalcTests.elm
+++ tests/StringCalcTests.elm
@@ -9,26 +9,29 @@ suite : Test
 suite =
     describe "The string calculator"
       [ test "should return 0 with empty string" <|\() ->
-          Expect.equal (StringCalc.add "") 0
+          Expect.equal (StringCalc.add "") (Result.Ok 0)
 
       , test "should return 1 with '1' as string" <|\() ->
-          Expect.equal (StringCalc.add "1") 1
+          Expect.equal (StringCalc.add "1") (Result.Ok 1)
 
       , test "should return 1234 with '1234' as string" <|\() ->
-          Expect.equal (StringCalc.add "1234") 1234
+          Expect.equal (StringCalc.add "1234") (Result.Ok 1234)
 
       , test "should return 3 with '1,2' as string" <|\() ->
-          Expect.equal (StringCalc.add "1,2") 3
+          Expect.equal (StringCalc.add "1,2") (Result.Ok 3)
 
       , test "should return 45 with '1,2,3,4,5,6,7,8,9' as string" <|\() ->
-          Expect.equal (StringCalc.add "1,2,3,4,5,6,7,8,9") 45
+          Expect.equal (StringCalc.add "1,2,3,4,5,6,7,8,9") (Result.Ok 45)
 
       , test "should return 6 with '1\n2,3' as string" <|\() ->
-          Expect.equal (StringCalc.add "1\n2,3") 6
+          Expect.equal (StringCalc.add "1\n2,3") (Result.Ok 6)
 
       , test "should return 3 with '//;\n1;2' as string" <|\() ->
-          Expect.equal (StringCalc.add "//;\n1;2") 3
+          Expect.equal (StringCalc.add "//;\n1;2") (Result.Ok 3)
 
       , test "should return 6 with '//$$\n1$$2$$3' as string" <|\() ->
-          Expect.equal (StringCalc.add "//$$\n1$$2$$3") 6
+          Expect.equal (StringCalc.add "//$$\n1$$2$$3") (Result.Ok 6)
+
+      , test "should return 'Negatives not allowed: -1' with '-1,2,3' as string" <|\() ->
+          Expect.equal (StringCalc.add "-1,2,3") (Result.Err "Negatives not allowed: -1")
       ]
